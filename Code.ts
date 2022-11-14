7fa7834f4ff590bb3952c2424938680917eab7af
Isaiah Davis <isaiah.davisx@gmail.com>
2022-11-13 21:16:45 -0600

    project starter

---
+++ Code.ts
@@ -0,0 +1,50 @@
+const calendarSheetURL = 'https://docs.google.com/spreadsheets/d/15Yyqt784HFmohsvc44Ewe_EiOdfw3khYdIE6vQJGWw4/edit#gid=0';
+
+function getAllCalendars(): GoogleAppsScript.Calendar.Calendar[] {
+  return CalendarApp.getAllCalendars();
+}
+
+function getSpreadsheetsAll(): GoogleAppsScript.Spreadsheet.Sheet[] {
+  return SpreadsheetApp.getActiveSpreadsheet().getSheets();
+}
+
+function getSheetsFromFile(fileIterator) {
+  let filesArray = [];
+  let fileObject = {};
+  let fileIndex = 0;
+
+  while (fileIterator.hasNext()) {
+    let file = fileIterator.next();
+    Logger.log(file.getName());
+  }
+}
+
+function getScriptAppByName(id): string {
+  return DriveApp.getFileById(id).getName();
+}
+
+function getScriptAppId(): string {
+  return ScriptApp.getScriptId();
+}
+
+function initialize(): void {
+  const calendars = getAllCalendars();
+  const calendarsSheet = SpreadsheetApp.openByUrl(calendarSheetURL);
+  const endDate = new Date();
+  const startDate = new Date();
+
+  function setToLastYear() {
+    startDate.setFullYear(endDate.getFullYear() - 1);
+  }
+
+  function getCalendarEvents() {
+    setToLastYear();
+    for (let x = 0; x < calendars.length; x++) {
+      calendars[x].getEvents(startDate, endDate)
+    }
+  }
+
+  // initialize
+  getCalendarEvents();
+  Logger.log(calendarsSheet.getSheets())
+}
